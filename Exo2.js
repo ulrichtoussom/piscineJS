1030c4b2491d3e54531984e8bd2c81d868ada17a
Dongo Fred <fredkamdem2209@gmail.com>
2026-01-21 10:49:15 +0100

    exo2

---
+++ Exo2.js
@@ -0,0 +1,6 @@
+function celsiusToFahrenheit(celsius) {
+    return celsius * 9 / 5 + 32;
+}
+console.log(celsiusToFahrenheit(0));
+console.log(celsiusToFahrenheit(100));
+console.log(celsiusToFahrenheit(-40));
