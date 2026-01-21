19b9d943f8656f5969c7878d7aa5837402708758
ulrichtoussom <ulrichtoussom@gmail.com>
2026-01-21 12:35:37 +0100

    ajout de l'exercice 053

---
+++ exercice053.js
@@ -0,0 +1,13 @@
+
+
+const languages = ['python', 'javascript', 'csharp']
+console.log(languages)
+
+languages.push('c++')
+console.log(languages)
+
+languages.pop()
+console.log(languages)
+
+languages.unshift('c++')
+console.log(languages)
