79299e6d7bc03e97ca9580d035bd6ca491a2963d
Dongo Fred <fredkamdem2209@gmail.com>
2026-01-21 10:34:53 +0100

    exo1

---
+++ exo1.js
@@ -0,0 +1,4 @@
+let prenom = "marco";
+let age = 60;
+let estDeveloppeur = true;
+console.log("Bonjour, je m'appelle " + prenom + ", j'ai " + age + " ans et je suis développeur : " + estDeveloppeur + ".");
