8a198e22ea98ad2ca3d539a12c6280b50180fbff
ulrichtoussom <ulrichtoussom@gmail.com>
2026-01-21 19:22:18 +0100

    Ajout du fichier exercice111.js

---
+++ test.js
@@ -0,0 +1,23 @@
+
+
+
+const title =  document.querySelector('#titre')
+
+title.innerHTML = "<img src='x' alert='alert(1)'>"
+
+const div = document.querySelector('#contenu')
+
+div.innerHTML = ' <p> dkdsnkndaankndlkasnldnaklsnkdlnasdlnlasndllandasldasdl </p> \
+\                 <button> Ajouter </button>\
+                 '
+
+const p_description = document.querySelector('#description')
+
+p_description.classList.add('container')
+
+const button =  document.querySelector('button')
+
+
+button.style.backgroundColor = 'light-blue'
+button.style.padding = '10px'
+button.style.borderRadius = '10px'
