a587aad5d732d3195b4eb5e2b0c68663de2481bc
Dongo Fred <fredkamdem2209@gmail.com>
2026-01-21 12:33:28 +0100

    exo051

---
+++ exo052.js
@@ -0,0 +1,5 @@
+let langages = ["JavaScript", "Python", "Java", "C++", "Ruby"];
+
+console.log("Premier élément :", langages[0]);
+console.log("Troisième élément :", langages[2]);
+console.log("Dernier élémennt :", langages[langages.length - 1]);
