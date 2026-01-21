663bd24ff7ea43ae5b9ee295733cb1473de4cd91
ulrichtoussom <ulrichtoussom@gmail.com>
2026-01-21 11:15:03 +0100

    ajout de l'exercice 023

---
+++ exercice023.js
@@ -0,0 +1,19 @@
+
+
+
+//const value =  parseInt(prompt('entrer un nombre'))
+
+const evenOrOdd = function(value){
+
+    if(value%2 == 0){
+        console.log('pair')
+        return 0
+    }
+    console.log('impair')
+}
+
+evenOrOdd(10)
+evenOrOdd(11)
+
+
+
