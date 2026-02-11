e8c386d768be649c2719274c96f814654f04ef39
Dongo Fred <fredkamdem2209@gmail.com>
2026-02-11 13:44:03 +0100

    actualisation de l'exo

---
+++ Exo001.js
@@ -0,0 +1,4 @@
+let prenom = "John";
+let age = 30;
+let estdeveloppeur = true;
+console.log("Bonjour, je m'appelle " + prenom + ", j'ai " + age + " ans et je suis développeur : " + estdeveloppeur + ".");
